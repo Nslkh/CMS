e3e8502a4b84602b3f5652a6eb51505acb666a59
Narzullo Salakhidinov <olluzran@gmail.com.com>
2021-01-09 15:54:52 -0700

    Creating DB

---
+++ includes/DB.php
@@ -0,0 +1,4 @@
+<?php 
+$DSN='mysql:host = localhost; dbname=cms1';
+$ConnectingDB = new PDO($DSN,'root','');
+?>
---
+++ includes/Sessions.php
@@ -0,0 +1,24 @@
+<?php  
+session_start();
+function ErrorMsg(){
+	if(isset($_SESSION['ErrorMsg'])) {
+		$Output = "<div class=\"alert alert-danger\">";
+		$Output .= htmlentities($_SESSION['ErrorMsg']) ;
+		$Output .= "</div>";
+		$_SESSION['ErrorMsg'] = null;
+		return $Output;
+
+	}
+}
+
+function SuccessMsg(){
+	if(isset($_SESSION['SuccessMsg'])) {
+		$Output = "<div class=\"alert alert-success\">";
+		$Output .= htmlentities($_SESSION['SuccessMsg']) ;
+		$Output .= "</div>";
+		$_SESSION['SuccessMsg'] = null;
+		return $Output;
+
+	}
+}
+?>
