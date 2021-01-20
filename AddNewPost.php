a93d176259c5a7516a4c4acdf265d378b0377229
Narzullo <olluzran@gmail.com>
2021-01-20 15:03:59 +0300

    testing the post page

--- AddNewPost.php
+++ AddNewPost.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once ("includes/DB.php");
 require_once ("includes/Functions.php");
 require_once ("includes/Sessions.php");
@@ -12,8 +12,7 @@ if(isset($_POST["Submit"])) {
 	date_default_timezone_set("Asia/Tashkent");
 	$CurrentTime = time();
 	
-	$DateTime = strftime("%B-%d-%Y %H:%M:%S", $CurrentTime); 
-
+	$DateTime = strftime("%B-%d-%Y %H:%M:%S", $CurrentTime);
 	if(empty($PostTitle)) {
 		$_SESSION["ErrorMsg"] = "Title cant be empty";
 		Redirect_to("AddNewPost.php");
@@ -38,7 +37,6 @@ if(isset($_POST["Submit"])) {
 		$stmt->bindValue(':PostDescription',$PostText);
 		$Execute=$stmt->execute();
 		move_uploaded_file($_FILES["Image"]["tmp_name"], $Target);
-
 		if($Execute){
 			$_SESSION["SuccessMsg"] = "Post  with id : ".$ConnectingDB->lastInsertId()."  Added Successfully";
 			Redirect_to("AddNewPost.php");
@@ -49,85 +47,77 @@ if(isset($_POST["Submit"])) {
 	}
 } //Ending of submit Button If-Condition
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie-edge">
-	<title>Categories</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/style.css">
-	
-	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-</head>
-<body>
-	<!-- NAVBAR -->
-	<div style="height:10px; background:#27aae1;"></div>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<div class="container">
-			<a href="#" class="navbar-brand">Narzullo.co</a>
-			<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarcollapseCMS" >
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie-edge">
+		<title>Categories</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+		
+		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+	</head>
+	<body>
+		<!-- NAVBAR -->
+		<div style="height:10px; background:#27aae1;"></div>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div class="container">
+				<a href="#" class="navbar-brand">Narzullo.co</a>
+				<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarcollapseCMS" >
 				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarcollapseCMS">
-			<ul class="navbar-nav mr-auto">
-				<li class="nav-item">
-					<a href="MyProfile.php" class="nav-link"> <i class="fas fa-user text-success"></i> My Profile</a>
-				</li>
-
-				<li class="nav-item">
-					<a href="Dashboard.php" class="nav-link">Dashboard</a>
-				</li>
-
-				<li class="nav-item">
-					<a href="Posts.php" class="nav-link">Post</a>
-				</li>
-
-				<li class="nav-item">
-					<a href="Categories.php" class="nav-link">Categories</a>
-				</li>
-
-				<li class="nav-item">
-					<a href="Admins.php" class="nav-link">Manage Admin</a>
-				</li>
-
-				<li class="nav-item">
-					<a href="Comments.php" class="nav-link">Comments</a>
-				</li>
-				<li class="nav-item">
-					<a href="Blog.php?page=1" class="nav-link">Live Blog</a>
-				</li>
-			</ul>
-			<ul class="navbar-nav ml-auto">
-				<li class="nav-item"  ><a href="logout.php" class="nav-link"> <i class="fas fa-user-times text-danger"></i> Logout</a></li>
-			</ul>
-		</div>
-		</div>
-	</nav>
-	<div style="height:10px; background:#27aae1;"></div>
-	<!-- NAVBAR END -->
-
-	<!-- HEADER -->
-	<header class="bg-dark text-white  py-3" >
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-				<h1> <i class="fas fa-edit" style="color: #27aae1;"></i>Add New Post</h1>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarcollapseCMS">
+					<ul class="navbar-nav mr-auto">
+						<li class="nav-item">
+							<a href="MyProfile.php" class="nav-link"> <i class="fas fa-user text-success"></i> My Profile</a>
+						</li>
+						<li class="nav-item">
+							<a href="Dashboard.php" class="nav-link">Dashboard</a>
+						</li>
+						<li class="nav-item">
+							<a href="Posts.php" class="nav-link">Post</a>
+						</li>
+						<li class="nav-item">
+							<a href="Categories.php" class="nav-link">Categories</a>
+						</li>
+						<li class="nav-item">
+							<a href="Admins.php" class="nav-link">Manage Admin</a>
+						</li>
+						<li class="nav-item">
+							<a href="Comments.php" class="nav-link">Comments</a>
+						</li>
+						<li class="nav-item">
+							<a href="Blog.php?page=1" class="nav-link">Live Blog</a>
+						</li>
+					</ul>
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item"  ><a href="logout.php" class="nav-link"> <i class="fas fa-user-times text-danger"></i> Logout</a></li>
+					</ul>
 				</div>
 			</div>
-		</div>
-	</header>
-	<!-- MAIN AREA -->
-
-	<section class="container py-2 mb-4">
-		<div class="row">
-			<div class="offset-lg-1 col-lg-10" style="min-height: 400px;">
-				<?php echo ErrorMsg();
-				echo SuccessMsg(); ?>
-				<form class="" action="AddNewPost.php" method="post" enctype="multipart/form-data">
-					 <div class="card bg-secondary text-light mb-3">
+		</nav>
+		<div style="height:10px; background:#27aae1;"></div>
+		<!-- NAVBAR END -->
+		<!-- HEADER -->
+		<header class="bg-dark text-white  py-3" >
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h1> <i class="fas fa-edit" style="color: #27aae1;"></i>Add New Post</h1>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- MAIN AREA -->
+		<section class="container py-2 mb-4">
+			<div class="row">
+				<div class="offset-lg-1 col-lg-10" style="min-height: 400px;">
+					<?php echo ErrorMsg();
+					echo SuccessMsg(); ?>
+					<form class="" action="AddNewPost.php" method="post" enctype="multipart/form-data">
+						<div class="card bg-secondary text-light mb-3">
 							<div class="card-body bg-dark">
 								<div class="form-group">
 									<label for="title"	><span class="FieldInfo">Post Title:</span></label>
@@ -150,51 +140,47 @@ if(isset($_POST["Submit"])) {
 								</div>
 								<div class="form-group mb-1">
 									<div class="custom-file">
-									<input class="custom-file-input" type="File" name="Image" id="imageSelect" value="">
-									<label for="imageSelect" class="custom-file-label">Select Image</label>
+										<input class="custom-file-input" type="File" name="Image" id="imageSelect" value="">
+										<label for="imageSelect" class="custom-file-label">Select Image</label>
+									</div>
 								</div>
-							</div>
-							<div class="form-group">
-								<label for="Post"><span class="FieldInfo">Post:</span></label>
-								<textarea class="form-control" name="PostDescription" id="Post" cols="30" rows="10"></textarea>
-							</div>
+								<div class="form-group">
+									<label for="Post"><span class="FieldInfo">Post:</span></label>
+									<textarea class="form-control" name="PostDescription" id="Post" cols="30" rows="10"></textarea>
+								</div>
 								<div class="row">
 									<div class="col-lg-6 mb-2">
 										<a href="Dashboard.php" class="btn btn-warning btn-block"><i class="fas fa-arrow-left"></i>Back to Dashboard </a>
 									</div>
 									<div class="col-lg-6 mb-2">
-									<button type="Submit" name="Submit" class="btn btn-success btn-block"><i class="fas fa-check"></i>Publish</button>
+										<button type="Submit" name="Submit" class="btn btn-success btn-block"><i class="fas fa-check"></i>Publish</button>
 									</div>
 								</div>
 							</div>
-					 </div>
-				</form>
+						</div>
+					</form>
+				</div>
 			</div>
-		</div>
-	</section>
-
-	<!-- MAIN AREA END-->
-	<!-- HEADER END -->
-
-	<!-- FOOTER -->
-	<footer class="bg-dark text-white">
-		<div class="container">
-			<div class="row">
-				<div class="col">
-				<p class="lead text-center">Made By | Narzullo Salakhidinov | <span id="year"></span> &copy; All right reserved </p>
-
+		</section>
+		<!-- MAIN AREA END-->
+		<!-- HEADER END -->
+		<!-- FOOTER -->
+		<footer class="bg-dark text-white">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<p class="lead text-center">Made By | Narzullo Salakhidinov.| <span id="year"></span> &copy; All right reserved </p>
+					</div>
+				</div>
 			</div>
-		</div>
-		</div>
-		<div style="height:10px; background:#27aae1;"></div>
-	</footer>
-	<!-- FOOTER END -->
-	<!-- jQuery and Bootstrap Bundle (includes Popper) -->
-	<script src="js/jquery-3.5.1.slim.min.js"></script>
-	<script src="js/bootstrap.bundle.min.js" ></script>
-	<script>
-		$('#year').text(new Date().getFullYear());
-	</script>
-
-</body>
+			<div style="height:10px; background:#27aae1;"></div>
+		</footer>
+		<!-- FOOTER END -->
+		<!-- jQuery and Bootstrap Bundle (includes Popper) -->
+		<script src="js/jquery-3.5.1.slim.min.js"></script>
+		<script src="js/bootstrap.bundle.min.js" ></script>
+		<script>
+			$('#year').text(new Date().getFullYear());
+		</script>
+	</body>
 </html>
--- Categories.php
+++ Categories.php
@@ -1,4 +1,4 @@
- <?php 
+<?php
 require_once ("includes/DB.php");
 require_once ("includes/Functions.php");
 require_once ("includes/Sessions.php");
@@ -8,20 +8,17 @@ if(isset($_POST["Submit"])) {
 	date_default_timezone_set("Asia/Tashkent");
 	$CurrentTime = time();
 	
-	$DateTime = strftime("%B-%d-%Y %H:%M:%S", $CurrentTime); 
-
+	$DateTime = strftime("%B-%d-%Y %H:%M:%S", $CurrentTime);
 	if(empty($Category)) {
 		$_SESSION["ErrorMsg"] = "All fields must be filled out";
 		Redirect_to("Categories.php");
 	}elseif(strlen($Category)<3) {
 		$_SESSION["ErrorMsg"] = "Category title should be greater than 2 characters";
 			Redirect_to("Categories.php");
-
 	}
 	elseif(strlen($Category)>49) {
 		$_SESSION["ErrorMsg"] = "Category title should be less than 50 characters";
 			Redirect_to("Categories.php");
-
 	}else{
 		//Query to insert category in DB when all is fine
 		global $ConnectingDB;
@@ -32,7 +29,6 @@ if(isset($_POST["Submit"])) {
 		$stmt->bindValue(':adminName',$Admin);
 		$stmt->bindValue('dateTime',$DateTime);
 		$Execute=$stmt->execute();
-
 		if($Execute){
 			$_SESSION["SuccessMsg"] = "Category  with id : ".$ConnectingDB->lastInsertId()."  Added Successfully";
 			Redirect_to("Categories.php");
@@ -43,130 +39,118 @@ if(isset($_POST["Submit"])) {
 	}
 } //Ending of submit Button If-Condition
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie-edge">
-	<title>Categories</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/style.css">
-	
-	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-</head>
-<body>
-	<!-- NAVBAR -->
-	<div style="height:10px; background:#27aae1;"></div>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<div class="container">
-			<a href="#" class="navbar-brand">Narzullo.co</a>
-			<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarcollapseCMS" >
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie-edge">
+		<title>Categories</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+		
+		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+	</head>
+	<body>
+		<!-- NAVBAR -->
+		<div style="height:10px; background:#27aae1;"></div>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div class="container">
+				<a href="#" class="navbar-brand">Narzullo.co</a>
+				<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarcollapseCMS" >
 				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarcollapseCMS">
-			<ul class="navbar-nav mr-auto">
-				<li class="nav-item">
-					<a href="MyProfile.php" class="nav-link"> <i class="fas fa-user text-success"></i> My Profile</a>
-				</li>
-
-				<li class="nav-item">
-					<a href="Dashboard.php" class="nav-link">Dashboard</a>
-				</li>
-
-				<li class="nav-item">
-					<a href="Posts.php" class="nav-link">Post</a>
-				</li>
-
-				<li class="nav-item">
-					<a href="Categories.php" class="nav-link">Categories</a>
-				</li>
-
-				<li class="nav-item">
-					<a href="Admins.php" class="nav-link">Manage Admin</a>
-				</li>
-
-				<li class="nav-item">
-					<a href="Comments.php" class="nav-link">Comments</a>
-				</li>
-				<li class="nav-item">
-					<a href="Blog.php?page=1" class="nav-link">Live Blog</a>
-				</li>
-			</ul>
-			<ul class="navbar-nav ml-auto">
-				<li class="nav-item"  ><a href="logout.php" class="nav-link"> <i class="fas fa-user-times text-danger"></i> Logout</a></li>
-			</ul>
-		</div>
-		</div>
-	</nav>
-	<div style="height:10px; background:#27aae1;"></div>
-	<!-- NAVBAR END -->
-
-	<!-- HEADER -->
-	<header class="bg-dark text-white  py-3" >
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-				<h1> <i class="fas fa-edit" style="color: #27aae1;"></i>Manage the Categories</h1>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarcollapseCMS">
+					<ul class="navbar-nav mr-auto">
+						<li class="nav-item">
+							<a href="MyProfile.php" class="nav-link"> <i class="fas fa-user text-success"></i> My Profile</a>
+						</li>
+						<li class="nav-item">
+							<a href="Dashboard.php" class="nav-link">Dashboard</a>
+						</li>
+						<li class="nav-item">
+							<a href="Posts.php" class="nav-link">Post</a>
+						</li>
+						<li class="nav-item">
+							<a href="Categories.php" class="nav-link">Categories</a>
+						</li>
+						<li class="nav-item">
+							<a href="Admins.php" class="nav-link">Manage Admin</a>
+						</li>
+						<li class="nav-item">
+							<a href="Comments.php" class="nav-link">Comments</a>
+						</li>
+						<li class="nav-item">
+							<a href="Blog.php?page=1" class="nav-link">Live Blog</a>
+						</li>
+					</ul>
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item"  ><a href="logout.php" class="nav-link"> <i class="fas fa-user-times text-danger"></i> Logout</a></li>
+					</ul>
 				</div>
 			</div>
-		</div>
-	</header>
-	<!-- MAIN AREA -->
-
-	<section class="container py-2 mb-4">
-		<div class="row">
-			<div class="offset-lg-1 col-lg-10" style="min-height: 400px;">
-				<?php echo ErrorMsg();
-				echo SuccessMsg(); ?>
-				<form class="" action="Categories.php" method="post">
-					 <div class="card bg-secondary text-light mb-3">
-					 	<div class="card-header">
-					 		<h1>Add New Category</h1>
-					 	</div>
-					 		<div class="card-body bg-dark">
-					 			<div class="form-group">
-					 				<label for="title"	><span class="FieldInfo">Categoty Title:</span></label>
-					 				<input class="form-control" type="text" name="CategoryTitle" id="title" placeholder="Type title here" value="">
-					 			</div>
-					 			<div class="row">
-					 				<div class="col-lg-6 mb-2">
-					 					<a href="Dashboard.php" class="btn btn-warning btn-block"><i class="fas fa-arrow-left"></i>Back to Dashboard </a>
-					 				</div>
-					 				<div class="col-lg-6 mb-2">
-					 				<button type="Submit" name="Submit" class="btn btn-success btn-block"><i class="fas fa-check"></i>Publish</button>
-					 				</div>
-					 			</div>
-					 		</div>
-					 </div>
-				</form>
-			</div>
-		</div>
-	</section>
-
-	<!-- MAIN AREA END-->
-	<!-- HEADER END -->
-
-	<!-- FOOTER -->
-	<footer class="bg-dark text-white">
-		<div class="container">
-			<div class="row">
-				<div class="col">
-				<p class="lead text-center">Made By | Narzullo Salakhidinov | <span id="year"></span> &copy; All right reserved </p>
-			</div>
-		</div>
-		</div>
+		</nav>
 		<div style="height:10px; background:#27aae1;"></div>
-
-	</footer>
-	<!-- FOOTER END -->
-	<!-- jQuery and Bootstrap Bundle (includes Popper) -->
-	<script src="js/jquery-3.5.1.slim.min.js"></script>
-	<script src="js/bootstrap.bundle.min.js" ></script>
-	<script>
-		$('#year').text(new Date().getFullYear());
-	</script>
-
-</body>
+		<!-- NAVBAR END -->
+		<!-- HEADER -->
+		<header class="bg-dark text-white  py-3" >
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h1> <i class="fas fa-edit" style="color: #27aae1;"></i>Manage the Categories</h1>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- MAIN AREA -->
+		<section class="container py-2 mb-4">
+			<div class="row">
+				<div class="offset-lg-1 col-lg-10" style="min-height: 400px;">
+					<?php echo ErrorMsg();
+					echo SuccessMsg(); ?>
+					<form class="" action="Categories.php" method="post">
+						<div class="card bg-secondary text-light mb-3">
+							<div class="card-header">
+								<h1>Add New Category</h1>
+							</div>
+							<div class="card-body bg-dark">
+								<div class="form-group">
+									<label for="title"	><span class="FieldInfo">Categoty Title:</span></label>
+									<input class="form-control" type="text" name="CategoryTitle" id="title" placeholder="Type title here" value="">
+								</div>
+								<div class="row">
+									<div class="col-lg-6 mb-2">
+										<a href="Dashboard.php" class="btn btn-warning btn-block"><i class="fas fa-arrow-left"></i>Back to Dashboard </a>
+									</div>
+									<div class="col-lg-6 mb-2">
+										<button type="Submit" name="Submit" class="btn btn-success btn-block"><i class="fas fa-check"></i>Publish</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</section>
+		<!-- MAIN AREA END-->
+		<!-- HEADER END -->
+		<!-- FOOTER -->
+		<footer class="bg-dark text-white">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<p class="lead text-center">Made By | Narzullo Salakhidinov. | <span id="year"></span> &copy; All right reserved </p>
+					</div>
+				</div>
+			</div>
+			<div style="height:10px; background:#27aae1;"></div>
+		</footer>
+		<!-- FOOTER END -->
+		<!-- jQuery and Bootstrap Bundle (includes Popper) -->
+		<script src="js/jquery-3.5.1.slim.min.js"></script>
+		<script src="js/bootstrap.bundle.min.js" ></script>
+		<script>
+			$('#year').text(new Date().getFullYear());
+		</script>
+	</body>
 </html>
